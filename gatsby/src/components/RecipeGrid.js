a60ab1238cec7178e8e8d01f984bdd01e93d065c
slaterslater <slaterslater@gmail.com>
2021-06-14 20:07:07 -0400

    add dark overlay and title onclick for each recipe

--- gatsby/src/components/RecipeGrid.js
+++ gatsby/src/components/RecipeGrid.js
@@ -11,11 +11,40 @@ const RecipeGridStyles = styled.div`
   grid-template-columns: repeat(3, 1fr);
   gap: 0.5rem;
   grid-auto-rows: auto auto;
+  a{
+    position:relative;
+  }
+  span{
+    position: absolute;
+    width:100%;
+    color: white;
+    top: -9999em;
+    z-index: 2;
+    text-align: center;
+  }
+  span.active {
+    top: 15px; 
+  }
 `
 
 const Whoops = styled.p`
   margin: auto;
 `
+
+const handleClick = e => {
+  const chosen = e.target.parentElement.previousElementSibling
+  if (chosen.classList.contains('active')) return
+  e.preventDefault()
+  const stale = document.querySelector('.active')
+  if (stale) {
+    stale.classList.remove('active')
+    stale.nextElementSibling.children[2].style.opacity = '1'
+  }
+  chosen.classList.add('active')
+  chosen.nextElementSibling.firstChild.style.backgroundColor = 'black'
+  chosen.nextElementSibling.children[2].style.opacity = '0.5'
+  // console.log(chosen.nextElementSibling.firstChild)
+}
 
 const RecipeGrid = ({ recipes }) => {
   if (recipes.length === 0)
@@ -24,10 +53,12 @@ const RecipeGrid = ({ recipes }) => {
     <RecipeGridStyles>
       {recipes.map(recipe => (
         <Link
+          onClick={handleClick}
           to={`/${recipe.slug.current}`}
           key={recipe.id}
           id={"recipe" + recipe.id}
         >
+          <span>{recipe.name}</span>
           <Img image={recipe.image.asset.gatsbyImageData} alt={recipe.name} />
         </Link>
       ))}
